a2c761074852703b5150f0562fb4932088460c28
connahKendrickMMU <44666644+connahKendrickMMU@users.noreply.github.com>
2020-02-25 10:31:46 +0000

    Initial commit of old website
    Uploading previous BMVC2019 website as a base line, will modify occordingly

---
+++ wp-content/uploads/hm_custom_css_js/custom0a32.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	document.querySelector(".sub-title").innerHTML = '9th—12th September 2019<br> 30th British Machine Vision Conference';
+});
