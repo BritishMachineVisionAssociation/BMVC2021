b1643c5ec1a6fc3ee8cbe5cad9be1f941387ac97
Adrian Davison <adie714@hotmail.com>
2020-03-04 18:20:11 +0000

    Changed references from bmvc2020.com to bmvc2020.github.io. This will need to be changed back once full website live.

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -3,13 +3,13 @@
     <service>
         <engineName>WordPress</engineName>
         <engineLink>https://wordpress.org/</engineLink>
-        <homePageLink>https://bmvc2020.com</homePageLink>
+        <homePageLink>https://bmvc2020.github.io</homePageLink>
         <apis>
-            <api name="WordPress" blogID="1" preferred="true" apiLink="https://bmvc2020.com/xmlrpc.php" />
-            <api name="Movable Type" blogID="1" preferred="false" apiLink="https://bmvc2020.com/xmlrpc.php" />
-            <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://bmvc2020.com/xmlrpc.php" />
-            <api name="Blogger" blogID="1" preferred="false" apiLink="https://bmvc2020.com/xmlrpc.php" />
-            <api name="WP-API" blogID="1" preferred="false" apiLink="https://bmvc2020.com/wp-json/" />
+            <api name="WordPress" blogID="1" preferred="true" apiLink="https://bmvc2020.github.io/xmlrpc.php" />
+            <api name="Movable Type" blogID="1" preferred="false" apiLink="https://bmvc2020.github.io/xmlrpc.php" />
+            <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://bmvc2020.github.io/xmlrpc.php" />
+            <api name="Blogger" blogID="1" preferred="false" apiLink="https://bmvc2020.github.io/xmlrpc.php" />
+            <api name="WP-API" blogID="1" preferred="false" apiLink="https://bmvc2020.github.io/wp-json/" />
         </apis>
     </service>
 </rsd>
