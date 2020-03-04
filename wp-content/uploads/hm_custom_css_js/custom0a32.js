fe49f30a695713d68086bea935b37f2b2819cc90
Adrian Davison <adie714@hotmail.com>
2020-03-04 17:08:29 +0000

    Made as many changes as possible to be in line with current BMVC 2020 information.

--- wp-content/uploads/hm_custom_css_js/custom0a32.js
+++ wp-content/uploads/hm_custom_css_js/custom0a32.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	document.querySelector(".sub-title").innerHTML = '7th—10th September 2020<br> 31st British Machine Vision Conference';
+	document.querySelector(".sub-title").innerHTML = '7th—11th September 2020<br> 31st British Machine Vision Conference';
 });
