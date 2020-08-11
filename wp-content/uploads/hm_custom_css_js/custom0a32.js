c04735348ddc06bcf8ad2eb70f304825b3956130
Neill D.F. Campbell <neill.campbell@cantab.net>
2020-08-11 11:39:10 +0100

    Remove old site.

--- wp-content/uploads/hm_custom_css_js/custom0a32.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	document.querySelector(".sub-title").innerHTML = '7th—11th September 2020<br> 31st British Machine Vision Conference';
-});
